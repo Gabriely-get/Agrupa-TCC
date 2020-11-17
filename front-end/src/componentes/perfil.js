f6127e9dfe220a10c13ece7e4feb8f4ee1034d26
NathanCoraArakaki <74389489+NathanCoraArakaki@users.noreply.github.com>
2020-11-17 00:55:12 -0300

    Add files via upload

--- front-end/src/componentes/perfil.js
+++ front-end/src/componentes/perfil.js
@@ -26,6 +26,125 @@ export default class Perfil extends React.Component {
                         <Link to="/" ><li><i class="fas fa-sign-out-alt"></i></li></Link>
                     </ul>
                 </header>
+                <section className="section-perfil">
+                        <img src="img/capa-exemplo.jpg" className="capa" />
+                        <img src="img/grupo-exemplo5.jpg" className="perfil" />
+                        <h1>Laura Figueiredo</h1>
+                        <h2>Apenas uma garota geek apaixonada pelo mundo dos animes, filmes e jogos :)</h2>
+                </section>
+                <section className="lista-grupos-perfil">
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo6.jpg" className="img" />
+                        <h1>Naruto</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Anime</p>
+                                <p>Shounen</p>
+                                <p>Ninja</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>15 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo7.jpg" className="img" />
+                        <h1>Valorant</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Jogo</p>
+                                <p>FPS</p>
+                                <p>Valorant</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>38 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo8.jpg" className="img" />
+                        <h1>Amantes de Pets</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Animais</p>
+                                <p>Pets</p>
+                                <p>Cachorro</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>61 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo9.jpg" className="img" />
+                        <h1>Futebol</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Esporte</p>
+                                <p>Futebol</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>40 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo10.jpg" className="img" />
+                        <h1>Dicas de Cozinha</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Colinária</p>
+                                <p>Cozinha</p>
+                                <p>Saúde</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>19 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo11.jpg" className="img" />
+                        <h1>Uniters</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>Música</p>
+                                <p>Pop</p>
+                                <p>Now United</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>23 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                    <div className="card-grupo">
+                        <img src="img/grupo-exemplo12.jpg" className="img" />
+                        <h1>Marvel</h1>
+                        <div className="info-grupo">
+                            <div className="tags">
+                                <p>HQ</p>
+                                <p>Filmes</p>
+                                <p>Marvel</p>
+                            </div>
+                            <div className="membros">
+                                <img src="img/logo.png" />
+                                <h3>11 membros</h3>
+                            </div>
+                            <a>Entrar</a>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
