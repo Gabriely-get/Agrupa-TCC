a614ec0757940aceeaf2a477d67bdf8790917147
Gabriely-get <65106495+Gabriely-get@users.noreply.github.com>
2020-12-07 16:49:10 -0300

    adiciona rota grupo index

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,15 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from 'cors';
 import {Routes} from "./routes/routes";
-// import {authRoutes} from "./routes/auth.routes";
 import { checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell } from "./middleware/verifySignUp";
-import {isAdmin, verifyToken} from "./middleware/authJwt";
-// import {checkDuplicateEmail} from '../src/middleware/verifySignUp';
+import {isAdmin, verifyToken, isGroupAdmin} from "./middleware/authJwt";
+import {checkCharacters, checkDuplicateGroupName} from "./middleware/verifyCreateGroup";
 
 createConnection().then(async connection => {
 
     // create express app
     const app = express();
     app.use(bodyParser.json());
-
     // setup express app here
     const corsOptions: cors.CorsOptions = {
         allowedHeaders: [
@@ -29,27 +27,25 @@ createConnection().then(async connection => {
             'Accept',
             'Accept-Encoding',
             'Connection',
-            'Authorization'
+            'X-Acess-Token'
         ],
         credentials: true,
         methods: 'GET, HEAD, OPRTIONS, PUT, PACH, POST, DELETE',
-        origin: 'http://localhost:3002',
-        preflightContinue: true
+        origin: 'http://localhost:3001',
+        preflightContinue: false
     };
-
+    
+    app.use(cors());
     app.use(cors(corsOptions));    
-
-    // Routes.forEach(middleware => {
-    //     [middleware.route];
-    //     });
-    // })
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
     // register express routes from defined application routes
     Routes.forEach(route => {
         //USER
         //user nao logado -> store USER
         if(route.method=='post' && route.route=='/users'){
-            (app as any)[route.method](route.route, checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell,(req: Request, res: Response, next: Function) => {
+            (app as any)[route.method](route.route, checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell, (req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
                 if (result instanceof Promise) {
                     result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
@@ -61,7 +57,7 @@ createConnection().then(async connection => {
         }
         //user logado update
         if(route.type=="userLogon" && route.method=="update"){
-            (app as any)[route.method](route.route, checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell, verifyToken,(req: Request, res: Response, next: Function) => {
+            (app as any)[route.method](route.route, checkDuplicateNames, checkDuplicateCell, verifyToken,(req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
                 if (result instanceof Promise) {
                     result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
@@ -73,7 +69,7 @@ createConnection().then(async connection => {
         }
         //user logado get user
         if(route.type=="userLogon" && route.method=="get"){
-            (app as any)[route.method](route.route,(req: Request, res: Response, next: Function) => {
+            (app as any)[route.method](route.route, verifyToken,(req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
                 if (result instanceof Promise) {
                     result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
@@ -83,7 +79,7 @@ createConnection().then(async connection => {
                 }
             });
         }
-        //admin logado
+        //admin logado -> 
         if(route.type=="authLogon" && !(route.method=="post")){
             (app as any)[route.method](route.route, verifyToken, isAdmin, (req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
@@ -94,9 +90,9 @@ createConnection().then(async connection => {
                     res.json(result);
                 }
             });
-        }
+        } //store Admin
         if(route.type=="authLogon" && route.method=="post"){
-            (app as any)[route.method](route.route, checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell, verifyToken, isAdmin, (req: Request, res: Response, next: Function) => {
+            (app as any)[route.method](route.route, checkDuplicateEmail, checkDuplicateNames, checkDuplicateCell, isAdmin, (req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
                 if (result instanceof Promise) {
                     result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
@@ -106,7 +102,18 @@ createConnection().then(async connection => {
                 }
             });
         }
-        //
+        //GROUP
+        if(route.type=="authGroupAction" && route.method=="put"){
+            (app as any)[route.method](route.route, verifyToken, isGroupAdmin, checkCharacters, checkDuplicateGroupName, (req: Request, res: Response, next: Function) => {
+                const result = (new (route.controller as any))[route.action](req, res, next);
+                if (result instanceof Promise) {
+                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+    
+                } else if (result !== null && result !== undefined) {
+                    res.json(result);
+                }
+            });
+        }
         else{
             (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
@@ -120,44 +127,9 @@ createConnection().then(async connection => {
         }
         
     });
-    
-    dotenv.config({
-        path: './.env'
-    });
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
 
     // start express server
-    app.listen(8000);
-
-    // insert new users for test
-    // await connection.manager.save(connection.manager.create(User, {
-    //     firstName: "Timber",
-    //     lastName: "Saw",
-    //     age: 27
-    // // }));
-    // const userRep = getRepository(User);
-    // await userRep.save({
-    //                      email: "gabs2@gmail.com",
-    //                      password: "999999999",
-    //                      firstName: "Willian",
-    //                      lastName: "Santos",
-    //                      birthDate: "2000/12/18",
-    //                      cellphone: 13991086543
-    //                    });
-
-                                // await connection.createQueryBuilder()
-                                //  .insert()
-                                //  .into(User)
-                                //  .values([{
-                                //       email: "gabs2@gmail.com",
-                                //      password: "999999999",
-                                //      firstName: "Willian",
-                                //      lastName: "Santos",
-                                //      birthDate: "2000/12/18",
-                                //      cellphone: 13991086543
-                                // }])
-                                //  .execute();
+    app.listen(3000);
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
