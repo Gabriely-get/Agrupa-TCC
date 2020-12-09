b803407068579f6aa97f1bf06388352ff2392a91
Gabriely-get <65106495+Gabriely-get@users.noreply.github.com>
2020-12-09 17:51:43 -0300

    Update perfil.js

--- front-end/src/componentes/perfil.js
+++ front-end/src/componentes/perfil.js
@@ -1,53 +1,93 @@
 import React from 'react';
 import './perfil.css';
 import './header.css';
+import api from "../services/api";
+import * as jwt from 'jsonwebtoken';
 import { Link } from 'react-router-dom';
+import { isAuthenticated, secret, getToken, campotoken } from '../services/auth';
 
 export default class Perfil extends React.Component {
+
+    state = {
+        nome: "",
+        nickname: "",
+        error: ""
+      };
+    
+    handleProfile = async e => {
+
+        e.preventDefault();
+
+        if(isAuthenticated){
+                console.log(localStorage.getItem(campotoken));
+                const token = localStorage.getItem(campotoken);
+                console.log(token);
+                jwt.verify(token, secret, async (err, decoded) => {
+                    if (err) {
+                        console.log("erro: " + err);
+                        this.setState({
+                            error:
+                            "Houve um erro mas não se preocupe, não é sua culpa"
+                        });
+                    } else{
+                        const id = decoded.id;
+                        const response = await api.get(`/users/${id}`);
+                        console.log(id);
+
+                        if(response.data.message == 'Usuario encontrado!'){
+                            this.setState({
+                                nome: 
+                                    response.data.username,
+                                nickname: 
+                                    '@'+response.data.nickname
+                                });
+                        } else{
+                            console.log('response.data');
+
+                            this.setState({
+                            error: 
+                                'no response data'
+                            });
+                        }
+                        }
+                    
+                });
+            
+        } else{
+            console.log('VAITOMANOCU ENT');
+        }
+    };
+
     render() {
         return (
-            <div>
-            <header className="menu-web">
-                <ul>
-                    <Link to="/grupos"><li><img src="img/logo.png" /></li></Link>
-                    <Link to="/grupos"><li>Início</li></Link>
-                    <Link><li><div className="i-grupos">Grupos<ul className="dropdown">
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo1.jpg" />Superonze</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo2.jpg" />Basqueteiros</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo3.jpg" />Fut Americano</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo4.jpg" />Overwatch</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo5.jpg" />Star Wars</li></Link>
-                    </ul><i class="fas fa-caret-down"></i></div></li></Link>
-                    <Link to="/eventos" ><li>Eventos</li></Link>
-                    <Link to="/sugestoes" ><li>Sugestões</li></Link>
-                </ul>
-                <ul>
-                    <Link to="/perfil" ><li><img src="img/icon-user.png" /></li></Link>
-                    <Link to="/" ><li><i class="fas fa-sign-out-alt"></i></li></Link>
-                </ul>
-            </header>
-            <header className="menu-mobile">
-                <input type="checkbox" className="checkbox-menu" />
-                <img src="img/menu-mobile.png" class="menu-hamburguer" />
-                <Link to="/grupos"><li><img src="img/logo-h-p.png" /></li></Link>
-                <ul className="menu-lateral">
-                    <Link to="/grupos"><li><img src="img/logo-h-p.png" /></li></Link>
-                    <Link to="/grupos"><li>Início</li></Link>
-                    <Link><li><div className="i-grupos">Grupos<ul className="dropdown">
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo1.jpg" />Superonze</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo2.jpg" />Basqueteiros</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo3.jpg" />Fut Americano</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo4.jpg" />Overwatch</li></Link>
-                        <Link to="/grupo" ><li><img src="img/grupo-exemplo5.jpg" />Star Wars</li></Link>
-                    </ul><i class="fas fa-caret-down"></i></div></li></Link>
-                    <Link to="/eventos" ><li>Eventos</li></Link>
-                    <Link to="/sugestoes" ><li>Sugestões</li></Link>
-                </ul>
-            </header>
+            <div onLoad={this.handleProfile}>
+                <header>
+                    <ul>
+                        <Link to="/grupos" ><li><img src="img/logo.png" /></li></Link>
+                        <Link to="/grupos"><li>Início</li></Link>
+                        <Link><li><div className="i-grupos">Grupos<ul className="dropdown">
+                            <Link to="/grupo" ><li><img src="img/grupo-exemplo1.jpg" />Superonze</li></Link>
+                            <Link to="/grupo" ><li><img src="img/grupo-exemplo2.jpg" />Basqueteiros</li></Link>
+                            <Link to="/grupo" ><li><img src="img/grupo-exemplo3.jpg" />Fut Americano</li></Link>
+                            <Link to="/grupo" ><li><img src="img/grupo-exemplo4.jpg" />Overwatch</li></Link>
+                            <Link to="/grupo" ><li><img src="img/grupo-exemplo5.jpg" />Star Wars</li></Link>
+                        </ul><i class="fas fa-caret-down"></i></div></li></Link>
+                        <Link to="/eventos"><li>Eventos</li></Link>
+                        <Link to="/sugestoes"><li>Sugestões</li></Link>
+                    </ul>
+                    <ul>
+                        <Link to="/perfil" ><li><img src="img/icon-user.png" /></li></Link>
+                        <Link to="/" ><li><i class="fas fa-sign-out-alt"></i></li></Link>
+                    </ul>
+                </header>
                 <section className="section-perfil">
                         <img src="img/capa-exemplo.jpg" className="capa" />
                         <img src="img/icon-user.png" className="perfil" />
-                        <h1>Laura Figueiredo</h1>
+                        <label display="block">{ this.state.error && <h3 id="loginError"> {this.state.error} </h3> }</label>
+                        <label display="block"> { this.state.nome && <h1 id="perfilNome"> {this.state.nome} </h1> } </label>
+                        <label display="block"> { this.state.nickname && <h2 id="perfilNome"> {this.state.nickname} </h2> } </label>
+                        {/* { this.state.nickName && <h2 id="perfilNickName"> {this.state.nickName} </h2> } */}
+                        {/* <h1>Laura Figueiredo</h1> */}
                         <h2>Apenas uma garota geek apaixonada pelo mundo dos animes, filmes e jogos :)</h2>
                 </section>
                 <section className="lista-grupos-perfil">
